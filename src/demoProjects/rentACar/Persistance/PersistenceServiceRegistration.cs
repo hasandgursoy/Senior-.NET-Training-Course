d8b7cd5e90926166eccabc89a6daf70eeda7ece7
hasandgursoy <hasan.d.gursoy@gmail.com>
2022-08-25 09:50:43 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Persistance/PersistenceServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using Application.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistance.Repositories;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+                                                                IConfiguration configuration)
+        {
+            services.AddDbContext<BaseDbContext>(options =>
+                                                     options.UseNpgsql(
+                                                         configuration.GetConnectionString("TemplateProjectConnectionString")));
+            services.AddScoped<IBrandRepository, BrandRepository>();
+
+            return services;
+        }
+    }
+}
